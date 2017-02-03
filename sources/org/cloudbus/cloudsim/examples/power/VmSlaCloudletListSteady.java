f00d3f61bd91e253836e0241594a518654e335e2
swethapts <swethapts@gmail.com>
2017-02-03 20:24:40 +0530

    sortByExpectedTime instead of deadline

--- sources/org/cloudbus/cloudsim/examples/power/VmSlaCloudletListSteady.java
+++ sources/org/cloudbus/cloudsim/examples/power/VmSlaCloudletListSteady.java
@@ -11,26 +11,30 @@ public class VmSlaCloudletListSteady {
 	private int mips;
 	private int sla;
 	private int cloudletId;
+	private int expectedTime;
 	
 	VmSlaCloudletListSteady(int type,int mips,int sla,int cloudletId){
 		this.type=type;
 		this.mips=mips;
 		this.sla=sla;
 		this.cloudletId=cloudletId;
+		this.expectedTime=(int)Math.ceil(cloudletId/mips);
 	}
 	public void addTuple(int type,int mips,int sla,int cloudletId){
 		this.type=type;
 		this.mips=mips;
 		this.sla=sla;
 		this.cloudletId=cloudletId;
+		this.expectedTime=(int)Math.ceil(cloudletId/mips);
 	}
 	
 	public int[] getTuple(){
-		int[] tuple = new int[4];
+		int[] tuple = new int[5];
 		tuple[0]=type;
 		tuple[1]=mips;
 		tuple[2]=sla;
 		tuple[3]=cloudletId;
+		tuple[4]=expectedTime;
 		return tuple;
 	}
 	public int getMips(){
@@ -41,6 +45,9 @@ public class VmSlaCloudletListSteady {
 	}
 	public int getCloudletId(){
 		return cloudletId;
+	}
+	public int getExpectedTime(){
+		return expectedTime;
 	}
 	public static <T extends VmSlaCloudletListSteady> void sortByMips(List<T> vmVmSlaCloudletListSteady) {
 		Collections.sort(vmVmSlaCloudletListSteady, new Comparator<T>() {
@@ -61,6 +68,28 @@ public class VmSlaCloudletListSteady {
 				Integer aMips = a.getMips();
 				Integer bMips = b.getMips();
 				return aMips.compareTo(bMips);
+			}
+		});
+	}
+	public static <T extends VmSlaCloudletListSteady> void sortByExpectedTime(List<T> vmVmSlaCloudletListSteady) {
+		Collections.sort(vmVmSlaCloudletListSteady, new Comparator<T>() {
+
+			@Override
+			public int compare(T a, T b) throws ClassCastException {
+				Integer aExpectedTime = a.getExpectedTime();
+				Integer bExpectedTime = b.getExpectedTime();
+				return bExpectedTime.compareTo(aExpectedTime);
+			}
+		});
+	}
+	public static <T extends VmSlaCloudletListSteady> void sortByExpectedTimeDescending(List<T> vmVmSlaCloudletListSteady) {
+		Collections.sort(vmVmSlaCloudletListSteady, new Comparator<T>() {
+
+			@Override
+			public int compare(T a, T b) throws ClassCastException {
+				Integer aExpectedTime = a.getExpectedTime();
+				Integer bExpectedTime = b.getExpectedTime();
+				return aExpectedTime.compareTo(bExpectedTime);
 			}
 		});
 	}
@@ -88,7 +117,7 @@ public class VmSlaCloudletListSteady {
 	}
 	public static <T extends VmSlaCloudletListSteady> void printList(List<T> vmVmSlaCloudletListSteady) {
 		for(VmSlaCloudletListSteady tuple : vmVmSlaCloudletListSteady){
-			System.out.println("\t"+tuple.getTuple()[0]+"\t"+tuple.getTuple()[1]+"\t"+tuple.getTuple()[2]+"\t"+tuple.getTuple()[3]);
+			System.out.println("\t"+tuple.getTuple()[0]+"\t"+tuple.getTuple()[1]+"\t"+tuple.getTuple()[2]+"\t"+tuple.getTuple()[3]+"\t"+tuple.getTuple()[4]);
 		}
 	}
 
